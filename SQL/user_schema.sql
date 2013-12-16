a5a08998a86fee991fcd40a41e85b6957cc2070f
Bohao Wang <bohwang@blackberry.com>
2013-12-16 17:10:16 -0500

    added user db schema

---
+++ SQL/user_schema.sql
@@ -0,0 +1,12 @@
+CREATE TABLE IF NOT EXISTS `users` (
+`userid` int(11) NOT NULL AUTO_INCREMENT,
+`username` varchar(32) NOT NULL UNIQUE,
+`fullname` varchar(50) NOT NULL,
+`email` varchar(100) NOT NULL,
+`password` varchar(100) NOT NULL,
+`salt` varchar(256) NOT NULL,
+`join_date` date NOT NULL,
+PRIMARY KEY (`userid`),
+UNIQUE (username),
+UNIQUE (email)
+)
